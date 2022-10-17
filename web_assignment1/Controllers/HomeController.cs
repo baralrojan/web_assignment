dce48a4776794b42205bb3e009babead0db3a809
rojan baral <baralrojan088@gmail.com>
2022-10-17 09:29:23 -0400

    Link Intro and controller Intro has created

--- web_assignment1/Controllers/HomeController.cs
+++ web_assignment1/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace web_assignment1.Controllers
         {
             return View();
         }
+        public IActionResult Intro()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
